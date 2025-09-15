58971702de5679ff7e3f5bf405775f29bc45d68a
anwar-bouchehboun <anouar.ab95@gmail.com>
2025-09-16 00:38:40 +0100

    config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,20 @@
 module.exports = {
-  apps: [{
-    name: 'projettps-dev',
-    script: 'npm',
-    args: 'run dev:host',
-    env: {
-      NODE_ENV: 'development'
-    }
-  }, {
-    name: 'projettps-prod',
-    script: 'npm',
-    args: 'run preview:host',
-    env: {
-      NODE_ENV: 'production'
-    }
-  }]
-}
+  apps: [
+    {
+      name: "projettps-dev",
+      script: "npm",
+      args: "run dev:host",
+      env: {
+        NODE_ENV: "development",
+      },
+    },
+    {
+      name: "projettps-prod",
+      script: "npm",
+      args: "run preview:host",
+      env: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
